c3c7cc348f953c1beae638e848bc64bdbc475c67
lcecatti <40144249+lcecatti@users.noreply.github.com>
2018-07-23 17:29:24 -0300

    Arquivos .SQL
    Nestes arquivos, sera mostrado como criar uma base de dados até a inserção/atualização e exclusão dos registros.
    
    A parte de select nas tabelas criadas será disponibilizado nos próximos dias.

---
+++ 03_InsertTables.sql
@@ -0,0 +1,24 @@
+USE [ViceriEcommerce]
+GO
+
+INSERT INTO [dbo].[CLIENTE_TB]
+           ([CODIGOCLIENTE]
+           ,[NOME]
+           ,[SOBRENOME]
+           ,[SEXO]
+           ,[DATANASC]
+           ,[CPF]
+           ,[EMAIL]
+           ,[TELEFONE])
+     VALUES
+           ('AAA12345'
+           ,'LUIZ CARLOS'
+           ,'CECATTI'
+           ,'M'
+           ,'1984/12/14'
+           ,'33908819806'
+           ,'LCECATTI@VICERI.COM.BR'
+           ,'11985658317')
+GO
+
+
